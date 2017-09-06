63e61b5f15a773007d5f7a542bfa4dd4ee8af7a8
Michael Bishop <miklb@miklb.com>
2017-09-06 11:23:00 -0400

    Updates

--- src/utils/SheltersApi.js
+++
@@ -1,9 +0,0 @@
-const api = "http://irma-api.herokuapp.com/api/v1/shelters"
-
-export const getAll = () =>
-  fetch(api)
-    .then(res => res.json())
-
-export const search = (query) =>
-  fetch(`${api}/?${query}`)
-    .then(res => res.json())
